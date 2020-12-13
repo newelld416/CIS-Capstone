9fb61e32de10acf386bdbd2ffec321a7c61663e2
newelld416 <aiute40@USWSA200279.local>
2020-12-13 12:25:14 -0500

    Minor updates

--- src/app/lesson/components/quiz/quiz.component.ts
+++ src/app/lesson/components/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LessonService } from '../../../shared/services/lesson.service';
 
